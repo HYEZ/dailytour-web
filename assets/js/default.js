7d4e3fecd1673c6d2ac506bdf27561aa0d7876f0
오혜지 <ohyeji@ohyejiui-MacBook-Pro.local>
2017-01-19 01:18:16 +0900

    update indexedDB form & js

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,7 +4,11 @@ function init(){
     LoadSvg();
     getJsonData();
     setMapLocatedColor();
+    initTestIndexedDB();
 }
+
+
+/* svg */
 
 function LoadSvg(){
 	$("#svg").load("/svg/map.svg", function(data){
@@ -65,6 +69,91 @@ function alertLocatedName(element){
     alert(self.name_ko);
 }
 
+
+
+/* IndexedDB */
+
+function initTestIndexedDB(){
+    var request = indexedDB.open("library"); //db name
+    const customerData = [
+        { id: "01", name: "Gopal K Varma", age: 35, email: "contact@tutorialspoint.com" },
+        { id: "02", name: "Prasad", age: 24, email: "prasad@tutorialspoint.com" }
+    ];
+    const customerData2 = [
+        {title: "4A44-44-asdfsadfasdf4444", name: "authAor", isbn: "bilAl@comdpany.com" },
+        {title: "4A44-44-sadfsadfasdf4444", name: "authAor", isbn: "bilAl@codmpany.com" }
+    ];
+    request.onsuccess = function() {
+        db = request.result;
+        var transaction = db.transaction("books", "readwrite"); // colum
+
+        var objectStore = transaction.objectStore("books");
+
+        objectStore.put({title: "4A44-44-sadfasdf4444", name: "authAor", isbn: "bilAl@company.com" });
+
+        transaction.oncomplete = function() {
+          // All requests have succeeded and the transaction has committed.
+        };
+        var index = objectStore.index("by_title");
+
+        var req = index.get("Quarry Memories");
+        req.onsuccess = function(){
+    //        alert(req.result.isbn);
+        }
+        for(i in customerData2) {
+            objectStore.put(customerData2[i]);
+        }
+        
+        deleteIndexedDB(db, "donna@home.org");
+        getAllIndexedDB(db);
+        
+        initTriggerindexedDB(db);
+    };
+}
+
+function initTriggerindexedDB(db){
+    $("body").on("click", "#indexedOk", function(){
+        setindexedDB(db, "insert");
+    });
+}
+
+function setindexedDB(db, type, index){
+    var objectStore = db.transaction("books","readwrite").objectStore("books");
+    
+    switch(type){
+        case "insert":
+            objectStore.put({title: "asdfa", name: "a", isbn: "ab" });
+            break;
+        case "delete":
+            objectStore.delete(index);
+            break;
+    }
+    
+    getAllIndexedDB(db);
+}
+
+function getAllIndexedDB(db){
+    var objectStore = db.transaction("books").objectStore("books");
+    objectStore.openCursor().onsuccess = function(event) {
+        var cursor = event.target.result;
+
+        if (cursor) {
+            console.log(cursor.key);
+//            alert("Name for id " + cursor.key + " is " + cursor.value.name + ", Age: " + cursor.value.age + ", Email: " + cursor.value.email);
+            cursor.continue();
+        } else {
+//            alert("No more entries!");
+        }
+        
+    };
+}
+
+
+/* init */
+
 $(function(){
 	init();
 });
+
+
+
--- page/client/indexeddb.php
+++ page/client/indexeddb.php
@@ -6,6 +6,30 @@
 <!-- Content -->
 <div class="container">
     <div class="row">
-        content
+        <div class="input-member-form">
+            <div class="form-group">
+                <label for="indexed_email">Email address</label>
+                <input type="email" class="form-control" id="indexed_email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="indexed_pw">Password</label>
+                <input type="password" class="form-control" id="indexed_pw" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <label for="indexed_name">Name</label>
+                <input type="text" class="form-control" id="indexed_name" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <input type="file" id="exampleInputFile">
+                <p class="help-block">Example block-level help text here.</p>
+            </div>
+            <div class="checkbox">
+                <label>
+                <input type="checkbox" id="indexed_check"> Check me out
+                </label>
+            </div>
+            <button type="button" id="indexedOk" class="btn btn-default">Submit</button>
+        </div>
     </div>
 </div>
